c6583621631251b843697fec80bccaa67aa2d2b4
Jamie Lee Rogers <156160941+JLRcodes@users.noreply.github.com>
2024-03-15 16:36:30 +1100

    Create getSize function

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+let color = "black";
+
 //DOMContentLoaded - Loads the html/css before the javascript
 document.addEventListener("DOMContentLoaded", function () {
-  createBoard(32);
-  console.log(board);
+  createBoard(16);
+
+  let btn_popup = document.querySelector("#popup");
+  btn_popup.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -14,7 +21,27 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "yellow";
+    //div.style.backgroundColor = "yellow";
+    div.addEventListener("mouseover", colorDiv());
     board.insertAdjacentElement("beforeend", div);
   }
+}
+
+function getSize() {
+  let input = prompt("What size board would you like?");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input > 100 || input <= 0) {
+    message.innerHTML = "Provide a number between 1 & 100";
+  } else {
+    message.innerHTML = "Success, Now you can play!";
+    return input;
+  }
+}
+
+function colorDiv() {}
+
+function setColor(colorChoice) {
+  let color = colorChoice;
 }
