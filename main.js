26e40e70acf92879063902c5eca0a9acbd9dd4da
Jamie Lee Rogers <156160941+JLRcodes@users.noreply.github.com>
2024-03-15 16:10:21 +1100

    Create createBoard function

---
+++ main.js
@@ -0,0 +1,20 @@
+//DOMContentLoaded - Loads the html/css before the javascript
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(32);
+  console.log(board);
+});
+
+function createBoard(size) {
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "yellow";
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
